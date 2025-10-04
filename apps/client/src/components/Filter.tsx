abda3393c9c4cf3d96a8607cdec6b55cdaa2df6e
mayank-gupta-03 <gupta.mayank2003@gmail.com>
2025-10-05 01:01:05 +0530

    Implement filter component

---
+++ apps/client/src/components/Filter.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+
+const Filter = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleFilter = (value: string) => {
+    const params = new URLSearchParams(searchParams);
+    value ? params.set("sort", value) : params.delete("sort");
+    router.push(`${pathname}?${params.toString()}`, { scroll: false });
+  };
+
+  return (
+    <div className="flex items-center justify-end gap-2 text-sm text-gray-500 my-6">
+      <span>Sort by:</span>
+      <select
+        name="sort"
+        id="sort"
+        className="ring-1 ring-gray-200 shadow-md p-1 rounded-sm"
+        onChange={(e) => handleFilter(e.target.value)}
+      >
+        <option value="">Select an option</option>
+        <option value="newest">Newest</option>
+        <option value="oldest">Oldest</option>
+        <option value="asc">Price: Low to High</option>
+        <option value="desc">Price: High to Low</option>
+      </select>
+    </div>
+  );
+};
+
+export default Filter;
