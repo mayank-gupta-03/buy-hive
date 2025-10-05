5c36ce9fad19e8261419dc73a0e239e8c0c0fee8
mayank-gupta-03 <gupta.mayank2003@gmail.com>
2025-10-05 21:08:54 +0530

    Separate out mock data

---
+++ apps/client/src/mock/category.data.tsx
@@ -0,0 +1,53 @@
+import { CategoryList } from "@/types/category.type";
+import {
+  ShoppingBasket,
+  Shirt,
+  Footprints,
+  Glasses,
+  Briefcase,
+  Venus,
+  Hand,
+} from "lucide-react";
+
+export const CATEGORIES: CategoryList = [
+  {
+    name: "All",
+    icon: <ShoppingBasket className="w-4 h-4" />,
+    slug: "all",
+  },
+  {
+    name: "T-shirts",
+    icon: <Shirt className="w-4 h-4" />,
+    slug: "t-shirts",
+  },
+  {
+    name: "Shoes",
+    icon: <Footprints className="w-4 h-4" />,
+    slug: "shoes",
+  },
+  {
+    name: "Accessories",
+    icon: <Glasses className="w-4 h-4" />,
+    slug: "accessories",
+  },
+  {
+    name: "Bags",
+    icon: <Briefcase className="w-4 h-4" />,
+    slug: "bags",
+  },
+  {
+    name: "Dresses",
+    icon: <Venus className="w-4 h-4" />,
+    slug: "dresses",
+  },
+  {
+    name: "Jackets",
+    icon: <Shirt className="w-4 h-4" />,
+    slug: "jackets",
+  },
+  {
+    name: "Gloves",
+    icon: <Hand className="w-4 h-4" />,
+    slug: "gloves",
+  },
+];
