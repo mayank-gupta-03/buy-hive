1b20cd3a9de906ee9fe01ca5d27c672f4af4664a
mayank-gupta-03 <gupta.mayank2003@gmail.com>
2025-10-23 06:54:44 +0530

    feat(product-service): Implement CRUD operations for categories

--- apps/product-service/src/controllers/category.controller.ts
+++ apps/product-service/src/controllers/category.controller.ts
@@ -1,7 +1,86 @@
 import { Request, Response } from "express";
+import { asyncHandler } from "../utils/asyncHandler";
+import { prisma, Prisma } from "@repo/product-db";
+import { ApiResponse } from "../utils/ApiResponse";
+import { ApiError } from "../utils/ApiError";
 
-export const getCategories = (req: Request, res: Response) => {};
-export const getCategoryById = (req: Request, res: Response) => {};
-export const createCategory = (req: Request, res: Response) => {};
-export const updateCategoryById = (req: Request, res: Response) => {};
-export const deleteCategoryById = (req: Request, res: Response) => {};
+export const getCategories = asyncHandler(
+  async (req: Request, res: Response) => {
+    const categories = await prisma.category.findMany();
+    const response = new ApiResponse(
+      200,
+      "Categories fetched successfully",
+      categories
+    );
+
+    return res.status(200).json(response);
+  }
+);
+
+export const getCategoryById = asyncHandler(
+  async (req: Request<{ id: string }>, res: Response) => {
+    const { id } = req.params;
+    const category = await prisma.category.findUnique({
+      where: { id },
+    });
+
+    if (!category) {
+      throw new ApiError(404, "Category not found");
+    }
+
+    const response = new ApiResponse(
+      200,
+      "Category fetched successfully",
+      category
+    );
+
+    return res.status(200).json(response);
+  }
+);
+
+export const createCategory = asyncHandler(
+  async (req: Request<{}, {}, Prisma.CategoryCreateInput>, res: Response) => {
+    const data = req.body;
+    const category = await prisma.category.create({ data });
+    const response = new ApiResponse(
+      201,
+      "Category created successfully",
+      category
+    );
+
+    return res.status(201).json(response);
+  }
+);
+export const updateCategoryById = asyncHandler(
+  async (req: Request<{ id: string }>, res: Response) => {
+    const { id } = req.params;
+    const data = req.body;
+    const category = await prisma.category.update({
+      where: { id },
+      data,
+    });
+    const response = new ApiResponse(
+      200,
+      "Category updated successfully",
+      category
+    );
+
+    return res.status(200).json(response);
+  }
+);
+
+export const deleteCategoryById = asyncHandler(
+  async (req: Request<{ id: string }>, res: Response) => {
+    const { id } = req.params;
+    const category = await prisma.category.delete({
+      where: { id },
+    });
+    const response = new ApiResponse(
+      200,
+      "Category deleted successfully",
+      category
+    );
+
+    return res.status(200).json(response);
+  }
+);
